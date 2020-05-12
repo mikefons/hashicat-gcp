965d35da48ae2b41f9d784f1e2ce2785ef4fe5ad
mikefons <fonseca.michael@gmail.com>
2020-05-12 13:37:56 -0400

    Update main.tf
    added tags

--- main.tf
+++ main.tf
@@ -39,6 +39,11 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
+  freeform_tags = {
+    billable = 'true"
+    department = "devops"
+  }
+
 
   boot_disk {
     initialize_params {
